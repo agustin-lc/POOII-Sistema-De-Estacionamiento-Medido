b7ec89c2914f7870ce681f4579cecb558f32fa05
agustin-lc <89219761+agustin-lc@users.noreply.github.com>
2024-05-23 00:30:29 -0300

    add usuario

---
+++ src/ar/edu/unq/po2/tpFinal/Usuario.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.po2.tpFinal;
+
+public class Usuario {
+	//private String nombre;
+	private Celular celular;
+	private Vehiculo vehiculo;
+
+}
+//Métodos:
+//    iniciarEstacionamiento()
+//    finalizarEstacionamiento()
