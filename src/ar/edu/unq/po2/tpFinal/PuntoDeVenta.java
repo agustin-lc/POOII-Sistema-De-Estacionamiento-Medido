36c32ab3d66f437c6168257290b40b3f18765b8a
Ivan Budzaj <budzajivan@gmail.com>
2024-05-23 20:21:40 -0300

    n

--- src/ar/edu/unq/po2/tpFinal/PuntoDeVenta.java
+++ src/ar/edu/unq/po2/tpFinal/PuntoDeVenta.java
@@ -2,9 +2,8 @@ package ar.edu.unq.po2.tpFinal;
 
 public class PuntoDeVenta {
 
-	public int uno = 1;
 
-	// ?
+
 	public void recargarCredito(Celular celular, double monto) {
 	    celular.recargarSaldo(monto);
 	    System.out.println("Se ha creditado el $" + monto);
