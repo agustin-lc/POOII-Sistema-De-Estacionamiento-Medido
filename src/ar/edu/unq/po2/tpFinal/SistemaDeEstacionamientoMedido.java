0e1f12456ae9f5f1a04787a15be8530b2bdef2d6
Ivan Budzaj <budzajivan@gmail.com>
2024-05-22 23:03:55 -0300

    comit

---
+++ src/ar/edu/unq/po2/tpFinal/SistemaDeEstacionamientoMedido.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.po2.tpFinal;
+
+public class SistemaDeEstacionamientoMedido {
+
+	
+	private int inicioFranja;
+	private int cierreFranja;
+	
+	
+	public int getInicioFranja() {
+		return inicioFranja;
+	}
+	public int getCierreFranja() {
+		return cierreFranja;
+	}
+	
+}
