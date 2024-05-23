04d7d58812f7fe0f6d3ea0afaff3fc97d4c385e2
Ivan Budzaj <budzajivan@gmail.com>
2024-05-22 21:35:35 -0300

    SEM creado

---
+++ src/SistemaDeEstacionamientoMedido.java
@@ -0,0 +1,16 @@
+
+public class SistemaDeEstacionamientoMedido {
+
+	
+	private int inicioFranja;
+	private int cierreFranja;
+	
+	
+	public int getInicioFranja() {
+		return inicioFranja;
+	}
+	public int getCierreFranja() {
+		return cierreFranja;
+	}
+	
+}
