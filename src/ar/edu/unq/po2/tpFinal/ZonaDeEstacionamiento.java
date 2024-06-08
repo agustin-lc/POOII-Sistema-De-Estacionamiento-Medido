12c1097dc2478fc86fb7fa9b681a41869fad34af
Ivan Budzaj <budzajivan@gmail.com>
2024-06-08 02:09:14 -0300

    estacionamietno borrado

--- src/ar/edu/unq/po2/tpFinal/ZonaDeEstacionamiento.java
+++ src/ar/edu/unq/po2/tpFinal/ZonaDeEstacionamiento.java
@@ -7,12 +7,10 @@ import java.util.List;
 public class ZonaDeEstacionamiento {
 
 	private List<PuntoDeVenta> puntosDeVentas;
-	private List<Estacionamiento> estacionamientos;
 	private Inspector inspector;
 
 	public ZonaDeEstacionamiento(Inspector inspec) {
 		this.puntosDeVentas = new ArrayList<PuntoDeVenta>();
-		this.estacionamientos = new ArrayList<Estacionamiento>();
 		this.inspector = inspec;
 	}
 
@@ -20,28 +18,13 @@ public class ZonaDeEstacionamiento {
 		return puntosDeVentas;
 	}
 
-	public List<Estacionamiento> getEstacionamientos() {
-		return estacionamientos;
-	}
+
 
 	public Inspector getInspector() {
 		return inspector;
 	}
 
-	public void iniciarEstacionamiento(Estacionamiento estacionamiento, Object object, int horas) {
-		// TODO Auto-generated method stub
-		if (estacionamientos.contains(estacionamiento)) {
 
-		}
-	}
 
-	public void agregarEstacionamiento(Estacionamiento estacionamiento) {
-		// TODO Auto-generated method stub
-		estacionamientos.add(estacionamiento);
-	}
-	
-	public boolean estacionamientoDePatente(String patente) {
-		return estacionamientos.contains(patente);
-	}
 
 }
