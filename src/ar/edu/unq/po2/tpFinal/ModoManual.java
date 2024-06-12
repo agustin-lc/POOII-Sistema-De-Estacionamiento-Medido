5e5e1a4553e817e64ec90b73b38055c8e76364c1
agustin-lc <89219761+agustin-lc@users.noreply.github.com>
2024-06-12 12:21:47 -0300

    Update ModoManual.java

--- src/ar/edu/unq/po2/tpFinal/ModoManual.java
+++ src/ar/edu/unq/po2/tpFinal/ModoManual.java
@@ -3,13 +3,19 @@ package ar.edu.unq.po2.tpFinal;
 public class ModoManual implements IModoApp {
 
 	@Override
-	public void alertaInicioDeEstacionamiento(Usuario usuario) {
-
-	}
+	public void inicioDeEstacionamiento(EstacionamientoViaApp estacionamientoApp) {
+		if (estacionamientoApp.getSaldo() > 40) {
+            		estacionamientoApp.getSem().notificacionDeInicio(estacionamientoApp.getPatente());
+            		System.out.println("Estacionamiento iniciado manualmente.");
+        	} else {
+            		System.out.println("Saldo insuficiente para iniciar el estacionamiento.");
+        	}
+    	}
 
 	@Override
-	public void alertaFinDeEstacionamiento(Usuario usuario) {
-
+	public void finDeEstacionamiento(EstacionamientoViaApp estacionamientoApp) {
+		estacionamientoApp.getSem().notificacionDeFin(estacionamientoApp.getCelular());
+        	System.out.println("Estacionamiento finalizado manualmente.");
 	}
 
 }
