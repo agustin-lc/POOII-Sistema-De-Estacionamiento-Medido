87614c5d1668caac86d2d1b46723efd0e80bdf17
agustin-lc <89219761+agustin-lc@users.noreply.github.com>
2024-06-12 12:20:22 -0300

    Update IModoApp.java

--- src/ar/edu/unq/po2/tpFinal/IModoApp.java
+++ src/ar/edu/unq/po2/tpFinal/IModoApp.java
@@ -1,7 +1,6 @@
 package ar.edu.unq.po2.tpFinal;
 
 public interface IModoApp {
-	public void alertaInicioDeEstacionamiento(Usuario usuario);
-
-	public void alertaFinDeEstacionamiento(Usuario usuario);
+	void inicioDeEstacionamiento(EstacionamientoViaApp estacionamientoApp);
+	void finDeEstacionamiento(EstacionamientoViaApp estacionamientoApp);
 }
