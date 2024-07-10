5cab47ede8b4d20013795820b162da141267816c
Ivan Budzaj <budzajivan@gmail.com>
2024-07-10 14:22:46 -0300

    Update EstadoMovimiento.java

--- src/ar/edu/unq/po2/tpFinal/EstadoMovimiento.java
+++ src/ar/edu/unq/po2/tpFinal/EstadoMovimiento.java
@@ -2,14 +2,15 @@ package ar.edu.unq.po2.tpFinal;
 public abstract class EstadoMovimiento {
 
 	public AppEstacionamiento app;
-	public abstract void walking();
-
-	public abstract void driving();
-
 	public void setApp(AppEstacionamiento app) {
 		// TODO Auto-generated method stub
 		this.app = app;
 	}
+	public abstract void walking();
+
+	public abstract void driving();
+
+	
 
 //	public abstract void driving(AppEstacionamiento app);
 //	public abstract void walking(AppEstacionamiento app);
